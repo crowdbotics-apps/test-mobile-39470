1e619bcbbe75fb24d340b479a2d65edb9b46ebad
 Zyggy Lau <zlau@qmo.io>
2023-03-27 12:35:16 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,7 +1,8 @@
 export const globalOptions = {
-  name: "test-mobile-39470",
-  url: "https://test-mobile-39470.botics.co",
-  api: "https://test-mobile-39470.botics.co/api/v1"
+  "name": "test-mobile-39470",
+  "url": "https://test-mobile-39470.botics.co",
+  "api": "https://test-mobile-39470.botics.co/api/v1",
+  "initialRoute": "AbouttheAppScreen6"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
